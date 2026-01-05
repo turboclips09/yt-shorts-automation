125a4fff0631393d6a4f3558f0c832bd5e4d4143
turboclips09 <turboclips09@gmail.com>
2026-01-06 05:14:39 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+topics = [
+    "fastest cars",
+    "illegal cars",
+    "supercars banned",
+    "luxury cars facts",
+    "cars that failed"
+]
+
+import random
+
+topic = random.choice(topics)
+
+print(f"This car fact will blow your mind. {topic}. And the last part changes everything.")
